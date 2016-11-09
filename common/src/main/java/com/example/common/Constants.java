f640d3c10230f23557ff6b878c3de0ce3eebdd62
Kinshuk <kinshuk.ram@gmail.com>
2016-11-10 03:19:52 +0530

    Common project to share the keys between mobile and wear project

---
+++ common/src/main/java/com/example/common/Constants.java
@@ -0,0 +1,12 @@
+package com.example.common;
+
+public class Constants {
+    //cant be instantiated
+    private Constants(){
+
+    }
+    public static final String WEATHER_PATH = "/weather";
+    public static final String KEY_HIGH_TEMP = "high_temp";
+    public static final String KEY_LOW_TEMP = "low_temp";
+    public static final String KEY_WEATHER_ID = "weather_id";
+}
